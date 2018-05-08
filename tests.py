aaa891b582310f170ae84945a25f7225637ea484
Brian Howald <bdhowald@gmail.com>
2018-05-07 23:01:16 -0400

    Adding other files.

---
+++ tests.py
@@ -0,0 +1,52 @@
+# # import tweepy
+# # import tweet_traffic_violations as ttv
+
+# # from mock import MagicMock
+
+# # class HowsMyDrivingTests:
+
+# # def test_handle_short_response():
+# #     message_id       = 123
+# #     message_type     = 'direct_message'
+# #     response_message = 'Some message'
+# #     username         = 'SomeCrazyPerson'
+
+# #     ttv.handle_short_response.assert_called_with('')
+
+# #     ttv.handle_short_response
+
+# #     # thing = ProductionClass()
+# #     # thing.method = MagicMock(return_value=3)
+# #     # thing.method(3, 4, 5, key='value')
+# #     # thing.method.assert_called_with(3, 4, 5, key='value')
+# #     mock = MagicMock()
+
+
+# # def main():
+# #     print('Beginning tests...')
+
+
+# #     test_handle_short_response()
+
+# # if __name__ == '__main__':
+# #     main()
+
+# import tweepy
+# import tweet_traffic_violations as ttv
+
+# from unittest import TestCase
+# from unittest.mock import patch, Mock
+
+
+# class TestSetUp(TestCase):
+#     @patch('tweepy.OAuthHandler', )
+
+#     def setUp(self):
+#         auth = tweepy.OAuthHandler('UzW1c1Gy4FmktSkfXF1dgfupr', '17Uf3vrk44m5fjTUEBPx8sPltX45OfZZtApWhWqt139O0GBgkV')
+#         auth.set_access_token('976593574732222465-YKv9y3mT9Vhu7Ufm7xkfk6Z2T3By3K9', 'B9dNrNT5io8GrB18Roy4eUSqDoR9YvJoErHKfT8aw3sGl')
+
+#         api = tweepy.API(auth)
+
+#     def test_set_up(self, MockApi):
+#         auth = MockApi()
+        
