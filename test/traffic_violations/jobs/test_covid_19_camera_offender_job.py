eddff0c9606445c226cb7a1c0964848a0ca2a2f0
Brian Howald <bdhowald@gmail.com>
2020-04-10 12:05:07 -0400

    Updating COVID-19 job to include speed camera data from March 31.

--- test/traffic_violations/jobs/test_covid_19_camera_offender_job.py
+++ test/traffic_violations/jobs/test_covid_19_camera_offender_job.py
@@ -110,14 +110,14 @@ class TestCovid19CameraOffenderJob(unittest.TestCase):
             if int(offender['count']) else '')
 
         covid_19_reckless_driver_string = (
-            'From March 10, 2020 to March 30, 2020, '
+            'From March 10, 2020 to March 31, 2020, '
             f"#{offender['state']}_{offender['plate']} "
             f"received {offender['count']} camera violations:\n\n"
             f'{speed_camera_violations_string}')
 
         dval_string = (
             'At this rate, this vehicle will receive '
-            f"{round(366.0/21 * int(offender['count']))} "
+            f"{round(366.0/22 * int(offender['count']))} "
             'speed safety camera violations over '
             'a year, qualifying it for towing or booting under '
             '@bradlander\'s Dangerous Vehicle Abatement Law and '
