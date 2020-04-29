9b43b1509be1325235bfb6b948c768c12d9736e2
Brian Howald <bdhowald@gmail.com>
2020-04-29 19:03:37 -0400

    Don't add TwitterEvents for @HowsMyDrivingNY messages.

--- traffic_violations/services/twitter_service.py
+++ traffic_violations/services/twitter_service.py
@@ -88,7 +88,7 @@ class TrafficViolationsTweeter:
 
             existing_event: Optional[TwitterEvent] = TwitterEvent.query.filter(TwitterEvent.event_id == message.id).first()
 
-            if not existing_event:
+            if not existing_event and message.user.id != HMDNY_TWITTER_USER_ID:
                 undetected_messages += 1
 
                 sender = senders[message.message_create['sender_id']]
@@ -127,7 +127,7 @@ class TrafficViolationsTweeter:
         for message in messages:
             existing_event: Optional[TwitterEvent] = TwitterEvent.query.filter(TwitterEvent.event_id == message.id).first()
 
-            if not existing_event:
+            if not existing_event and message.user.id != HMDNY_TWITTER_USER_ID:
                 undetected_messages += 1
 
                 event: TwitterEvent = TwitterEvent(
