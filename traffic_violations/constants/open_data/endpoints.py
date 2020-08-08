4b6b71c24a814f708039edc6994d2f300d446071
Brian Howald <bdhowald@gmail.com>
2020-08-07 21:29:20 -0400

    Adding FY 2021 endpoint (which again happens to be the previous fiscal year's endpoint...)

--- traffic_violations/constants/open_data/endpoints.py
+++ traffic_violations/constants/open_data/endpoints.py
@@ -5,7 +5,8 @@ FISCAL_YEAR_DATABASE_ENDPOINTS = {
   2017: 'https://data.cityofnewyork.us/resource/2bnn-yakx.json',
   2018: 'https://data.cityofnewyork.us/resource/a5td-mswe.json',
   2019: 'https://data.cityofnewyork.us/resource/faiq-9dfq.json',
-  2020: 'https://data.cityofnewyork.us/resource/pvqr-7yc4.json'
+  2020: 'https://data.cityofnewyork.us/resource/p7t3-5i9s.json',
+  2021: 'https://data.cityofnewyork.us/resource/pvqr-7yc4.json',
 }
 
 MEDALLION_ENDPOINT = 'https://data.cityofnewyork.us/resource/rhe8-mgbb.json'
